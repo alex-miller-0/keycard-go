ee6297d989cc5a6e42cdb86c7de1d8be409bde06
Alex Miller <asmiller1989@gmail.com>
2019-06-21 09:34:35 -0500

    Adds types module logger

--- types/types.go
+++ types/types.go
@@ -1,7 +1,9 @@
 package types
 
 import "github.com/alex-miller-0/keycard-go/apdu"
+import "github.com/ethereum/go-ethereum/log"
 
+var logger = log.New("package", "types")
 // Channel is an interface with a Send method to send apdu commands and receive apdu responses.
 type Channel interface {
 	Send(*apdu.Command) (*apdu.Response, error)
